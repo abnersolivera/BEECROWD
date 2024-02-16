349f649b2fb7d38387bb52c0285623d70621bc54
Abner <abnersanto2014@gmail.com>
2024-02-16 00:38:13 -0300

    feat:Desafio Soma Simples

---
+++ 1003_soma_simples/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SimpleSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"SOMA = {ReadIntegerInput("Enter the first number: ") + ReadIntegerInput("Enter the second number: ")}");
+    }
+
+    static int ReadIntegerInput(string message)
+    {
+        do
+        {
+            Console.Write(message);
+
+            if (int.TryParse(Console.ReadLine(), out int result))
+                return result;
+
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        } while (true);
+    }
+}
