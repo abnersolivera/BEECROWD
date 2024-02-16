cec20bd65a98ab963a4e6e8d7ba88f4ed3144950
Abner <abnersanto2014@gmail.com>
2024-02-16 00:43:31 -0300

    feat:Desafio Diferença

---
+++ 1007_diferenca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"DIFERENCA = {DifferenceCalculation(ReadIntegerInput("A"), ReadIntegerInput("B"), ReadIntegerInput("C"), ReadIntegerInput("D"))}");
+        }
+
+        static int ReadIntegerInput(string message)
+        {
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out int result))
+                    return result;
+
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            } while (true);
+        }
+
+        static int DifferenceCalculation(int a, int b, int c, int d)
+        {
+            return a * b - c * d;
+        }
+    }
+}
