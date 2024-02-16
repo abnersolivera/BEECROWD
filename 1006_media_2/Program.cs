2e80972d811f29b0bb87a079ec556ef1ecc0a253
Abner <abnersanto2014@gmail.com>
2024-02-16 00:41:36 -0300

    feat:Desafio Média 2

---
+++ 1006_media_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AverageTwo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        double c = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+        Console.WriteLine($"MEDIA = {Average(a, b, c):F1}");
+    }
+
+    static double Average(double a, double b, double c)
+    {
+        double average = ((a * 2) + (b * 3) + (c * 5)) / 10.0;
+
+        if (average >= 10.0)
+            return 10.0;
+
+        return average;
+    }
+}
