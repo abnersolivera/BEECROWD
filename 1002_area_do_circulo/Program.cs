7a09fdbde0999d5176f84f7238e48d5516c643d6
Abner <abnersanto2014@gmail.com>
2024-02-16 00:36:26 -0300

    feat:Desafio Área do Círculo

---
+++ 1002_area_do_circulo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AreaOfCircle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (double.TryParse(Console.ReadLine(), out double ray))        
+            Console.WriteLine($"A={(3.14159 * Math.Pow(ray, 2)).ToString("F4")}");        
+        else        
+            Console.WriteLine("Invalid input. Please enter a valid number.");        
+    }
+}
