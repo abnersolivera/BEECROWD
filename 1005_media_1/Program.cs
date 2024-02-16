86a27ebfd595f195022704dbbe3a51af9d1f2fdf
Abner <abnersanto2014@gmail.com>
2024-02-16 00:40:25 -0300

    feat:Desafio Média 1

---
+++ 1005_media_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AverageOne;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal a = decimal.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        decimal b = decimal.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+        Console.WriteLine($"MEDIA = {Average(a, b):F5}");
+    }
+
+    static decimal Average(decimal a, decimal b)
+    {
+        decimal average = ((a * 3.5m) + (b * 7.5m)) / 11.00000m;
+
+        if (average >= 10.00000m)        
+            return 10.00000m;        
+
+        return average;
+    }
+}
